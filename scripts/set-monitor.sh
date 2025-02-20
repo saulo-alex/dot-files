8cb02227efc9f6a8dffc0901ccf21689fffa10b9
Saulo Alex <saulod2@gmail.com>
2025-02-20 12:08:56 -0300

    Melhorado script para definir o modo do monitor

--- scripts/set-monitor.sh
+++ scripts/set-monitor.sh
@@ -1,11 +1,37 @@
 #!/usr/bin/env bash
 
-modes=( $(echo ~/.screenlayout/mode-*) )
-descriptions=("Modo de tela com dois monitores" "Modo de tela com apenas monitor externo" "Modo de tela com apenas monitor interno")
-modes_count=${#modes[*]}
-current=$(cat ~/.local/share/current-mode-monitor)
-new_current=$(( (current+1) % modes_count ))
-${modes[$new_current]}
-notify-send -i /usr/share/icons/Humanity/apps/24/preferences-desktop-display.svg -u low -t 5000 -e -r 1010 "${descriptions[$new_current]}"
-echo "$new_current" > ~/.local/share/current-mode-monitor
+mode_count=3
+mode_files=( $(echo ~/.screenlayout/mode-*.sh) )
+mode_descriptions=("Modo de tela com dois monitores" "Modo de tela com apenas monitor externo" "Modo de tela com apenas monitor interno")
+current_index_mode=0
+user_action=$1
+notify_option=$2
+
+function send_notification() {
+    notify-send -i /usr/share/icons/Humanity/apps/24/preferences-desktop-display.svg -u low -t 5000 -e -r 1010 "${mode_descriptions[$current_index_mode]}"
+}
+
+function set_next_mode() {
+    local -i last_index_mode=$(cat ~/.local/share/last-monitor-mode)
+    current_index_mode=$(( (last_index_mode + 1) % mode_count ))
+    echo "$current_index_mode" > ~/.local/share/last-monitor-mode
+    ${mode_files[$current_index_mode]}
+}
+
+function set_last_mode() {
+    local -i last_index_mode=$(cat ~/.local/share/last-monitor-mode)
+    current_index_mode=$last_index_mode
+    ${mode_files[$current_index_mode]}
+}
+
+if [ "$user_action" == "next" ]; then
+    set_next_mode
+    [ "$notify_option" == "notify" ] && send_notification 
+else
+#   user_action=last
+    set_last_mode
+    [ "$notify_option" == "notify" ] && send_notification
+fi
+
+exit 0
 
